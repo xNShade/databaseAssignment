311b69350921837dad02db84a14834205758f592
Ryan <ryanxredheart@gmail.com>
2019-04-27 20:06:27 -0500

    Issue with cloning.. fixed.

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 //jshint esversion:6
 
 const express = require("express");
-//const bodyParser = require("body-parser"); //Deprecated
-//use app.use(bodyParser.json());
-//and app.use(bodyParser.urlencoded({extended: true}));
+const bodyParser = require("body-parser"); //Deprecated
 var mysql = require("mysql");
 
 const app = express();
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 //----------- Ryan - section to connect to DB
 var con = mysql.createConnection({
-  host: "localhost",    // ip address of server running mysql
-  user: "user1",         // user name to your mysql database
-  password: "password",   // corresponding password
+  host: "localhost", // ip address of server running mysql
+  user: "user1", // user name to your mysql database
+  password: "password", // corresponding password
   database: "smal_mart"
 });
 
@@ -24,21 +25,44 @@ con.connect(function(err) {
 
   console.log('Connected to the MySQL server.');
 });
-
-con.end();
 //--------- Ryan - end DB connection section
-app.get("/", function(req, res){
-  res.render('index',{});
+
+
+app.get("/", function(req, res) {
+  res.render('index', {});
 });
 
-app.get("/login", function(req,res){
-  res.render('login',{});
+app.get("/login", function(req, res) {
+  res.render('login', {});
 });
 
-app.get("/Store1", function(req,res){
-  res.render('store1',{});
+app.post("/login",function(req,res){
+  var user = req.body.uname;
+  var password = req.body.upassword;
+  //Query database to see if username and password match
+  // if username and password match
+  // tell them thanks.
+  // else
+  // tell them invalid Login
+  //Probably will just keep this part super simple.
 });
 
-app.listen(3000, function(){
+app.get("/store", function(req, res) {
+  res.render('store', {});
+});
+
+app.post("/store", function(req, res) {
+  var storeget = req.body.storeselect;
+  var itemcheck = req.body.searchitem;
+  // You can con.query the storeget variable here to pass into the page
+  // And query the itemcheck variable here to pass into the page if needed.
+  if (itemcheck === "") {
+    //This will be the browsing of a store
+  } else {
+    //This will browse the store for the specific item
+  }
+});
+
+app.listen(3000, function() {
   console.log("Server started on port:3000");
 });
